c48da954c64936b9f967d25b54aa58343e072549
Ali Suboh <ali.suboh@hyperpay.com>
2022-08-03 02:48:33 +0300

    .

--- app/Observers/EmployeeObserver.php
+++ app/Observers/EmployeeObserver.php
@@ -27,6 +27,7 @@ class EmployeeObserver
             $admin->name = $providersEmployee->full_name;
             $admin->phone_number = $providersEmployee->phone_number;
             $admin->driver_id = $providersEmployee->id;
+            $admin->provider_id = $providersEmployee->provider_id;
             $admin->api_token = Str::random(60);
             $admin->save();
             $roles = new AdminRoleUsers(['user_id'=>$admin->id, 'role_id'=>3]);
