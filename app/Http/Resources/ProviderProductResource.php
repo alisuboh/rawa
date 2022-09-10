ebb37bb32130335902d0e8d60465dde93abae2a2
Ali Suboh <ali.suboh@hyperpay.com>
2022-09-10 19:07:39 +0300

    pp

--- app/Http/Resources/ProviderProductResource.php
+++ app/Http/Resources/ProviderProductResource.php
@@ -9,18 +9,12 @@ class ProviderProductResource extends MainResource
     public function combinedAttrs()
     {
         return [
-            "provider_product_id" => $this->provider_product_id,
-            "provider_id" => $this->provider_id,
-            "product_id" => $this->product_id,
-            "provider_product_name" => $this->provider_product_name,
+            "id" => $this->provider_product_id,
+            "name" => $this->provider_product_name,
             "price" => $this->price,
             "measurement" => $this->measurement,
-            "is_active" => $this->is_active,
             "discount" => $this->discount,
-            "icon_path" => $this->icon_path,
-            "created_at" => $this->created_at,
-            "updated_at" => $this->updated_at,
-
+            "img" => $this->icon_path
         ];
     }
 }
