df6e1016db0916bf97027a06627ed77f659f5652
Ali Suboh <ali.suboh@hyperpay.com>
2022-09-10 20:25:33 +0300

    .

--- app/Http/Resources/ProviderProductResource.php
+++ app/Http/Resources/ProviderProductResource.php
@@ -3,19 +3,21 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Storage;
 
 class ProviderProductResource extends MainResource
 {
     public function combinedAttrs()
     {
+        
         return [
             "id" => $this->provider_product_id,
             "name" => $this->provider_product_name,
             "price" => $this->price,
             "measurement" => $this->measurement,
             "discount" => $this->discount,
-            "img" => Storage::url($this->icon_path)
+            "img" => App::make('url')->to(Storage::url($this->icon_path))
         ];
     }
 }
