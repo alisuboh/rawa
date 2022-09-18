49b605b66a746c439b58f6f6cfa5b15c5839d04a
Ali Suboh <ali.suboh@hyperpay.com>
2022-09-18 22:53:13 +0300

    .

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -4,16 +4,28 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class CustomerResource extends JsonResource
+class CustomerResource extends MainResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
+    public function combinedAttrs()
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "user_name" => $this->user_name,
+            "mobile_number" => $this->mobile_number,
+            "phone" => $this->mobile_number,
+            "email" => $this->email,
+            "password" => $this->password,
+            "has_branches" => $this->has_branches,
+            "default_provider_id" => $this->default_provider_id,
+            "can_recive_any_time" => $this->can_recive_any_time,
+            "on_days" => $this->on_days,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+            "location_lat" => $this->location_lat,
+            "location_lng" => $this->location_lng,
+            "address_description" => $this->address_description,
+
+        ];
     }
 }
