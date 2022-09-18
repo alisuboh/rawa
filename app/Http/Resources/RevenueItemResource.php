dea45bbb8ee2f8b726431480bb571d2acc4ca758
Ali Suboh <ali.suboh@hyperpay.com>
2022-09-18 21:39:03 +0300

    rev

--- app/Http/Resources/RevenueItemResource.php
+++ app/Http/Resources/RevenueItemResource.php
@@ -8,10 +8,24 @@ class RevenueItemResource extends MainResource
 {
     public function combinedAttrs()
     {
-        return [
+        // dd(request()->path());
+        if(strpos(request()->path(),'revenue/')){
+            return [
                 'transaction_date' => $this->transaction_date??$this->created_at,
                 "revenue_parant" => $this->revenueCategory->revenueParant->id??null,
                 "revenue_category" => $this->revenueCategory->id??null,
+                'total_price' => $this->total_price,
+                "description" => $this->description,
+                //customer
+                //customer name
+                'bond_no' => $this->bond_no,                
+                "id" => $this->id,
+        ];
+        }
+        return [
+                'transaction_date' => $this->transaction_date??$this->created_at,
+                "revenue_parant" => $this->revenueCategory->revenueParant->name??"لا يوجد",
+                "revenue_category" => $this->revenueCategory->description??"لا يوجد",
                 'total_price' => $this->total_price,
                 // "description" => $this->description,
                 //customer
