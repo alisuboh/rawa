a58085c65f05d3dda27d1a431eec409c557287a2
Ali Suboh <ali.suboh@hyperpay.com>
2022-09-27 14:16:34 +0300

    edit

--- app/Http/Resources/RevenueItemResource.php
+++ app/Http/Resources/RevenueItemResource.php
@@ -16,7 +16,7 @@ class RevenueItemResource extends MainResource
                 'total_price' => $this->total_price??'',
                 "description" => $this->description??'',
                 'customer_id' => $this->customer_id,
-                'customer_name' => $this->customer->name,
+                'customer_name' => $this->customer?$this->customer->name:'',
                 'bond_no' => $this->bond_no??'',                
                 "id" => $this->id
         ];
@@ -26,7 +26,7 @@ class RevenueItemResource extends MainResource
                 "revenue_parant" => $this->revenueCategory->revenueParant->name??"لا يوجد",
                 "revenue_category" => $this->revenueCategory->description??"لا يوجد",
                 'total_price' => $this->total_price??"لا يوجد",
-                'customer_name' => $this->customer->name??'',
+                'customer_name' => $this->customer?$this->customer->name:'لا يوجد',
                 'bond_no' => $this->bond_no??"لا يوجد",   
                 'created_at' => $this->created_at->format('Y-m-d H:i:s'),            
                 "id" => $this->id
