d19756d49a7e2695a0e0ecf48df8aba0de62cca0
Ali Suboh <ali.suboh@hyperpay.com>
2022-10-08 23:28:32 +0300

    .

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,15 +9,20 @@ use Illuminate\Support\Facades\Storage;
 class MainController extends Controller
 {
     public function index(){
+        $myfile = fopen('storage/.well-known/pki-validation/AF5501DA92567D7EE44AAFC356BB1D26.txt', "r");
+print_r(fread($myfile,filesize('storage/.well-known/pki-validation/AF5501DA92567D7EE44AAFC356BB1D26.txt')));
+fclose($myfile);
 
-        $content = App::make('url')->to(Storage::url('.well-known/pki-validation/AF5501DA92567D7EE44AAFC356BB1D26.txt'));
-        $filename = 'AF5501DA92567D7EE44AAFC356BB1D26.txt';
-        $headers = array(
-            'Content-Type' => 'plain/txt',
-            'Content-Disposition' => sprintf('attachment; filename="%s"', $filename),
-            'Content-Length' => sizeof($content),
-        );
-        return response($content,200,$headers);
+// dd('asas');
+        // $content = Storage::url('.well-known/pki-validation/AF5501DA92567D7EE44AAFC356BB1D26.txt'));
+        // // dd($content);
+        // $filename = 'AF5501DA92567D7EE44AAFC356BB1D26.txt';
+        // $headers = array(
+        //     'Content-Type' => 'plain/txt',
+        //     'Content-Disposition' => sprintf('attachment; filename="%s"', $filename),
+        //     'Content-Length' => sizeof($content),
+        // );
+        // return response($content,200,$headers);
         // return response('hello world')->header('Content-Type', 'text/plain');
 
     }
