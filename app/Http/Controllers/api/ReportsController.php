38620d8787a993b9c0320c939b16308c93b33aeb
Ali Suboh <ali.suboh@hyperpay.com>
2022-10-15 19:44:33 +0300

    supplier

--- app/Http/Controllers/api/ReportsController.php
+++ app/Http/Controllers/api/ReportsController.php
@@ -4,14 +4,10 @@ namespace App\Http\Controllers\api;
 
 use App\Constants\TransCode;
 use App\Http\Controllers\Controller;
-use App\Models\Customer;
 use App\Models\CustomerOrder;
 use App\Models\ExpenseItem;
-use App\Models\ProvidersEmployee;
 use App\Models\Purchase;
 use App\Models\RevenueItem;
-use App\Models\Supplier;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
