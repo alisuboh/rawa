0ae32d97355eba0323e844de694427655dc6bd0e
Ali Suboh <ali.suboh@hyperpay.com>
2022-10-29 19:33:01 +0200

    .

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -64,11 +64,11 @@ class Trip extends Model
             // dd(CustomerOrder::find([ 'id' => $order['orders_id']])->first()->toArray());
             try{
                 if(!empty(auth()->user()->driver_id)){
-                    if($model = CustomerOrder::where('status', '=', 1)->with('customer','address')->find([ 'id' => $order['orders_id']])->first())
-                    $orders[$key] = $model->toArray();
+                    if($model = CustomerOrder::where('status', '=', "1")->where( 'id' ,"=", $order['orders_id'])->with('customer','address')->first())
+                        $orders[$key] = $model->toArray();
                 }else
-                if($model = CustomerOrder::with('customer','address')->find([ 'id' => $order['orders_id']])->first())
-                    $orders[$key] = $model->toArray();
+                    if($model = CustomerOrder::with('customer','address')->find([ 'id' => $order['orders_id']])->first())
+                        $orders[$key] = $model->toArray();
 
             }catch(\Exception $e){
                 return [];
